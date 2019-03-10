8b6b49ef2220b9a916d258a1cdf6d9bae7a10d60
qawefqfwqf <38812647+qawefqfwqf@users.noreply.github.com>
2019-03-10 22:20:47 +0800

    h

---
+++ ssh-demo/src/main/java/com/zzh/action/HelloAction.java
@@ -0,0 +1,22 @@
+package com.zzh.action;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@ParentPackage("default")
+@Action(value = "hello", results = { @Result(name = "success", type = "dispatcher", location = "/index.jsp") })
+@Namespace("/")
+public class HelloAction extends ActionSupport {
+
+	public void say() {
+		System.out.println("===========================");
+	}
+
+	// public void test() {
+	// System.out.println("调用了TestAction里面的test方法");
+	// }
+}

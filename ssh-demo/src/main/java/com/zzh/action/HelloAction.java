960baa25cd12129048bcdb5ec752f5e1416dbca1
qawefqfwqf <38812647+qawefqfwqf@users.noreply.github.com>
2019-03-12 00:55:58 +0800

    struts2

--- ssh-demo/src/main/java/com/zzh/action/HelloAction.java
+++ ssh-demo/src/main/java/com/zzh/action/HelloAction.java
@@ -7,16 +7,17 @@ import org.apache.struts2.convention.annotation.Result;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-@ParentPackage("default")
-@Action(value = "hello", results = { @Result(name = "success", type = "dispatcher", location = "/index.jsp") })
-@Namespace("/")
+//@ParentPackage("default")
+//@Action(value = "hello", results = { @Result(name = "success", type = "dispatcher", location = "/index.jsp") })
+//@Namespace("/")
 public class HelloAction extends ActionSupport {
 
-	public void say() {
-		System.out.println("===========================");
-	}
-
-	// public void test() {
-	// System.out.println("调用了TestAction里面的test方法");
+	// public void say() {
+	// System.out.println("===========================");
 	// }
+
+	public String test() {
+		System.out.println("调用了HelloAction里面的test方法");
+		return "ok";
+	}
 }
---
+++ ssh-demo/src/main/java/com/zzh/action/UserAction.java
@@ -0,0 +1,23 @@
+package com.zzh.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+
+	public String find() {
+		return SUCCESS;
+	}
+
+	public String add() {
+		return SUCCESS;
+	}
+
+	public String update() {
+		return SUCCESS;
+	}
+
+	public String delete() {
+		return SUCCESS;
+	}
+
+}

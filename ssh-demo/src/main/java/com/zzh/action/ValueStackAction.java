7cdff069b8910c4ca8ea287845eaa32b5b7746d9
qawefqfwqf <38812647+qawefqfwqf@users.noreply.github.com>
2019-03-19 22:53:18 +0800

    ssh

---
+++ ssh-demo/src/main/java/com/zzh/action/ValueStackAction.java
@@ -0,0 +1,80 @@
+package com.zzh.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.ValueStack;
+import com.zzh.entity.UserEntity;
+
+@ParentPackage("struts-default")
+@Namespace("/vs")
+public class ValueStackAction extends ActionSupport {
+
+	private String hello;
+
+	public String getHello() {
+		return hello;
+	}
+
+	private UserEntity user = new UserEntity();
+
+	public UserEntity getUser() {
+		return user;
+	}
+
+	private List<UserEntity> list = new ArrayList<>();
+
+	public List<UserEntity> getList() {
+		return list;
+	}
+
+	@Action(value = "/hello", results = { @Result(location = "/valueStack.jsp") })
+	public String testString() {
+		hello = "hello world";
+
+		return "success";
+	}
+
+	@Action(value = "/object", results = { @Result(location = "/valueStack.jsp") })
+	public String testObject() {
+
+		user.setUsername("zzh");
+		user.setPassword("123456");
+
+		return "success";
+	}
+
+	@Action(value = "/list", results = { @Result(location = "/valueStack.jsp") })
+	public String testList() {
+
+		for (int i = 0; i < 10; i++) {
+			UserEntity user = new UserEntity();
+			user.setUsername("zzh");
+			user.setPassword("123456");
+
+			list.add(user);
+		}
+
+		return "success";
+	}
+
+	@Action(value = "/other", results = { @Result(location = "/valueStack.jsp") })
+	public String testOther() {
+
+		ValueStack vs = ActionContext.getContext().getValueStack();
+
+		vs.set("setTest", "setTest");
+
+		vs.push("pushTest");
+
+		return "success";
+	}
+
+}
---
+++ ssh-demo/src/main/webapp/valueStack.jsp
@@ -0,0 +1,52 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!-- 引入el标识所需要的标签 -->
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="s" uri="/struts-tags"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet"
+	href="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/css/bootstrap.min.css">
+<script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+<script
+	src="https://cdn.staticfile.org/popper.js/1.12.5/umd/popper.min.js"></script>
+<script
+	src="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<style type="text/css">
+div {
+	display: block;
+}
+</style>
+</head>
+<body>
+	<s:debug></s:debug>
+
+	<s:property value="hello" />
+
+	<s:property value="user.username" />
+	<s:property value="user.password" />
+
+	<s:property value="list[0].username" />
+	<s:property value="list[1].password" />
+
+
+	<s:iterator value="list">
+		<div>
+			<s:property value="username" />
+			<s:property value="password" />
+		</div>
+	</s:iterator>
+
+	<div>
+		<s:property value="setTest" />
+	</div>
+
+	<div>
+		<s:property value="[0].top" />
+	</div>
+
+</body>
+</html>
